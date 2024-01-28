dccae5db5a5d28d97f875ea62d5bee6b8a9d0c2d
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-01-28 23:04:41 +0400

    Update array

--- scenes/pipes/pipe.gd
+++ scenes/pipes/pipe.gd
@@ -5,11 +5,10 @@ extends Area2D
 @onready var connected_pipes = []
 
 func _ready() -> void:
-	add_to_group("pipes")
 	area_entered.connect(_on_area_entered)
 	area_exited.connect(_on_area_exited)
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if has_overlapping_areas():
 		fill_pipe()
 	else:
@@ -19,27 +18,28 @@ func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if empty.get_rect().has_point(to_local(event.position)):
 			rotate_pipe()
-			print(connected_pipes)
-
-func rotate_pipe():
-	self.rotation += 1.5708
+		var adjacent_pipes = get_overlapping_areas()
+		for adjacent_pipe in adjacent_pipes:
+			if adjacent_pipe not in connected_pipes:
+				connected_pipes.append(adjacent_pipe)
+		print(connected_pipes)
+		
 
 func _on_area_entered(area: Area2D) -> void:
-	if area in get_tree().get_nodes_in_group("pipes"):
-		var adjacent_pipes = get_overlapping_areas()
-		if adjacent_pipes not in connected_pipes:
-			connected_pipes.append(adjacent_pipes)
-		if area not in connected_pipes:	
+	if area not in connected_pipes:
 			connected_pipes.append(area)
 
 func _on_area_exited(area: Area2D) -> void:
 		if area in connected_pipes:
 			connected_pipes.erase(area)
 
+func rotate_pipe():
+	rotation_degrees += 90
+
 func fill_pipe():
 		empty.hide()
 		full.show()
-#
+		
 func empty_pipe():
 		empty.show()
 		full.hide()
