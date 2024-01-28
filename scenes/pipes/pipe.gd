c9896c3f875659937d0f990b35305525ee63bbc5
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-01-28 16:24:32 +0400

    Update collision shapes

--- scenes/pipes/pipe.gd
+++ scenes/pipes/pipe.gd
@@ -3,12 +3,15 @@ extends Area2D
 @onready var empty: Sprite2D = $Empty
 @onready var full: Sprite2D = $Full
 
+@export var current_pipe := false
+
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 	area_exited.connect(_on_area_exited)
 	
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		current_pipe = true
 		if empty.get_rect().has_point(to_local(event.position)):
 			rotate_pipe()
 
@@ -24,9 +27,11 @@ func rotate_pipe():
 	self.rotation += 1.5708
 
 func _on_area_entered(area: Area2D) -> void:
+	if current_pipe:
 		link_pipes()
 
 func _on_area_exited(area: Area2D) -> void:
+	if current_pipe:
 		unlink_pipes()
 
 #	var overlap = has_overlapping_areas()
