8bd3bfe3562a27a03b6c45adede9e80d04b087ee
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-01-28 21:58:10 +0400

    Try to add pipes to array

--- scenes/pipes/pipe.gd
+++ scenes/pipes/pipe.gd
@@ -2,35 +2,44 @@ extends Area2D
 
 @onready var empty: Sprite2D = $Empty
 @onready var full: Sprite2D = $Full
+@onready var connected_pipes = []
 
 func _ready() -> void:
+	add_to_group("pipes")
 	area_entered.connect(_on_area_entered)
 	area_exited.connect(_on_area_exited)
-
+	
 func _process(delta: float) -> void:
 	if has_overlapping_areas():
-		empty.hide()
-		full.show()
+		fill_pipe()
+	else:
+		empty_pipe()
 	
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if empty.get_rect().has_point(to_local(event.position)):
 			rotate_pipe()
-
-func link_pipes():
-		empty.hide()
-		full.show()
-	
-func unlink_pipes():
-		empty.show()
-		full.hide()
+			print(connected_pipes)
 
 func rotate_pipe():
 	self.rotation += 1.5708
 
 func _on_area_entered(area: Area2D) -> void:
-		link_pipes()
+	if area in get_tree().get_nodes_in_group("pipes"):
+		var adjacent_pipes = get_overlapping_areas()
+		if adjacent_pipes not in connected_pipes:
+			connected_pipes.append(adjacent_pipes)
+		if area not in connected_pipes:	
+			connected_pipes.append(area)
 
 func _on_area_exited(area: Area2D) -> void:
-		unlink_pipes()
+		if area in connected_pipes:
+			connected_pipes.erase(area)
 
+func fill_pipe():
+		empty.hide()
+		full.show()
+#
+func empty_pipe():
+		empty.show()
+		full.hide()
