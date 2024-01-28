86a2f15da9791329ed829ba2439aa5a749de5e4c
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-01-28 14:08:22 +0400

    Update pipe collision shapes

--- scenes/pipes/pipe.gd
+++ scenes/pipes/pipe.gd
@@ -1,30 +1,34 @@
-extends Node2D
+extends Area2D
 
 @onready var empty: Sprite2D = $Empty
 @onready var full: Sprite2D = $Full
 
 func _ready() -> void:
-	for child in get_children():
-		if child.has_signal("connected"):
-			child.connected.connect(link_pipes)
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
 	
-	for child in get_children():
-		if child.has_signal("disconnected"):
-			child.disconnected.connect(unlink_pipes)
-
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if empty.get_rect().has_point(to_local(event.position)):
-			print("A click!")
 			rotate_pipe()
 
 func link_pipes():
-	empty.hide()
-	full.show()
+		empty.hide()
+		full.show()
 	
 func unlink_pipes():
-	empty.show()
-	full.hide()
+		empty.show()
+		full.hide()
 
 func rotate_pipe():
 	self.rotation += 1.5708
+
+func _on_area_entered(area: Area2D) -> void:
+		link_pipes()
+
+func _on_area_exited(area: Area2D) -> void:
+		unlink_pipes()
+
+#	var overlap = has_overlapping_areas()
+#	if overlap:
+#		print("overlap")
