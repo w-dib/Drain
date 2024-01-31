ad293bd702a6b86a7ffbfd8c2b570c7fb7dc6b3c
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-01-31 20:18:25 +0400

    Narrow down printing bug

--- scripts/pipe_end.gd
+++ scripts/pipe_end.gd
@@ -11,15 +11,15 @@ func _ready() -> void:
 	area_exited.connect(_on_area_exited)
 		
 func _on_area_entered(area: Area2D) -> void:
-	Global.connected_pipes.append(self)
-	if full.visible and area not in Global.connected_pipes:
-			Global.connected_pipes.append(area)
+	Global.connected_pipes.append(self.owner)
+	if full.visible and area.owner not in Global.connected_pipes:
+			Global.connected_pipes.append(area.owner)
 	fill.emit()
 	
 func _on_area_exited(area: Area2D) -> void:
-	if self in Global.connected_pipes:
-		Global.connected_pipes.erase(self)
-	if area in Global.connected_pipes:
-		Global.connected_pipes.erase(area)
+	if self.owner in Global.connected_pipes:
+		Global.connected_pipes.erase(self.owner)
+	if area.owner in Global.connected_pipes:
+		Global.connected_pipes.erase(area.owner)
 	if not overlaps_area(area):
 		empty.emit()
